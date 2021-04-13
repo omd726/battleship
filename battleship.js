b53801db13b5ec97667b22e7b227eda1e52e2919
omd <omd726@gmail.com>
2021-04-13 17:42:28 +0800

    finish controller gprocessGuess function

--- battleship.js
+++ battleship.js
@@ -41,7 +41,7 @@ var model = {
 				view.displayMessage("HIT!");
 				if (this.isSunk(ship)) {
 					view.displayMessage("You sank my battleship!!");
-					this.shipSunk++;
+					this.shipsSunk++;
 				}
 				return true;
 			}
@@ -62,15 +62,55 @@ var model = {
 	}
 };
 
+var controller = {
 
+	guesses: 0,
 
+	processGuess: function(guess) {
+		var location = parseGuess(guess);
+		if (location) {
+			this.guesses++;
+			var hit = model.fire(location);
+			if (hit && model.shipsSunk === model.numShips) {
+				view.displayMessage("You sank all my ships in " + this.guesses + " guesses");
+			}
+		}
+	}
+};
 
-model.fire("53");
-model.fire("06");
-model.fire("16");
-model.fire("26");
-model.fire("34");
-model.fire("24");
-model.fire("44");
-model.fire("12");
-model.fire("11");
+function parseGuess(guess) {
+	
+	var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+
+	if (guess === null || guess.length !== 2) {
+		alert("Oops, please enter a letter and a number on the board!");
+	} else {
+		firstChar = guess.charAt(0);
+		var row = alphabet.indexOf(firstChar);
+		var column = guess.charAt(1);
+
+		if (isNaN(row) || isNaN(column)) {
+			alert("Oops, that isn't on the board");
+		} else if ( row < 0 || row >= model.boardSize || 
+					column < 0 || column >= model.boardSize ) {
+			alert("Oops, that's off the board");
+		} else {
+			return row + column;
+		}
+	}
+	return null;
+};
+
+controller.processGuess("A0");
+
+controller.processGuess("A6");
+controller.processGuess("B6");
+controller.processGuess("C6");
+
+controller.processGuess("C4");
+controller.processGuess("D4");
+controller.processGuess("E4");
+
+controller.processGuess("B0");
+controller.processGuess("B1");
+controller.processGuess("B2");
